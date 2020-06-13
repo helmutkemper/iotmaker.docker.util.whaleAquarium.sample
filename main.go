b41620fbd3a30c6342601b9c6ac47151eafeecb1
helmut Kemper <helmut.kemper@gmail.com>
2020-06-13 18:19:27 -0300

    Update main.go

--- main.go
+++ main.go
@@ -1,25 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+  "fmt"
+  "net/http"
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "hello\n")
-}
-
-func headers(w http.ResponseWriter, req *http.Request) {
-	for name, headers := range req.Header {
-		for _, h := range headers {
-			fmt.Fprintf(w, "%v: %v\n", name, h)
-		}
-	}
+  fmt.Fprintf(w, "Hello World\n")
 }
 
 func main() {
-	http.HandleFunc("/hello", hello)
-	http.HandleFunc("/headers", headers)
-
-	http.ListenAndServe(":8080", nil)
+  http.HandleFunc("/", hello)
+  _ = http.ListenAndServe(":8080", nil)
 }
