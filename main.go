045dba79be07991c7769ba784874bfc770cd78b2
helmut Kemper <helmut.kemper@gmail.com>
2020-06-14 17:24:19 -0300

    Update main.go

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+  "fmt"
+  "io/ioutil"
   "log"
   "net/http"
 )
 
 func main() {
-  fs := http.FileServer(http.Dir("/app/static/"))
-  http.Handle("/", fs)
+  //fs := http.FileServer(http.Dir("./static"))
+  //http.Handle("/", fs)
+  http.HandleFunc("/dir", func(w http.ResponseWriter, r *http.Request) {
+    d, _ := ioutil.ReadDir("./")
+    for _, v := range d {
+      fmt.Printf("%v<br>\n", v.Name())
+    }
+  })
 
   err := http.ListenAndServe(":3000", nil)
   if err != nil {
